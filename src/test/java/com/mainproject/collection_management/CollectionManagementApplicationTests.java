19b11a0ab19697289a8bc0ec250f15859fa77588
MdTanvirHossainTusher <md.tanvir.hossain.tusher@gmail.com>
2024-08-01 16:30:31 +0600

    hello world controller added

---
+++ src/test/java/com/mainproject/collection_management/CollectionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainproject.collection_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
